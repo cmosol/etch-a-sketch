d71c17af878f7e12c278bfb60684493605cdb90d
Christopher Mosol <cmosol121@gmail.com>
2022-01-05 19:17:37 -0600

    add clear, color/bw mode, earaser functionality

--- hover.js
+++ hover.js
@@ -1,10 +1,23 @@
+let mode = "BW"
+
 const squares = document.querySelectorAll('.square');
 squares.forEach((square) => {
     square.addEventListener('mouseover',function(){toggleColor(square)});
 });
 
 function toggleColor(square){
-    square.style.backgroundColor=rgbGenerator();
+    if (square.id==='clear' && mode==='BW'){
+        square.setAttribute('id','bw');
+        square.style.backgroundColor="#000000";
+    }
+    if (square.id==='clear' && mode==='COLOR') {
+        square.setAttribute('id','color');
+        square.style.backgroundColor=rgbGenerator();
+    }
+    if (square.id!=='clear' && mode==='ERASER'){
+        square.setAttribute('id','clear');
+        square.style.backgroundColor="#dbdbdb"
+    }
 }
 
 function rgbGenerator() {
@@ -14,3 +27,31 @@ function rgbGenerator() {
     const rgb = "rgb("+x+","+y+","+z+")";
     return rgb;
 }
+
+function init() {
+    const btns = document.querySelectorAll('button');
+    const squares = document.querySelectorAll('.square');
+    btns.forEach((button) => {
+        button.addEventListener('click', function () {
+            console.log("in button event");
+            if (button.id==='clear') {
+                console.log("in clear")
+                squares.forEach((square) =>{
+                    square.style.backgroundColor="#dbdbdb";
+                    square.setAttribute('id','clear');
+                });
+            }
+            if (button.id==="bw") {
+                mode="BW";
+            }
+            if (button.id==="color") {
+                mode = "COLOR";
+            }
+            if (button.id==="eraser") {
+                mode = "ERASER";
+            }
+        });
+    });
+}
+
+init();
